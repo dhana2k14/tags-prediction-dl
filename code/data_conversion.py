15570b2cab58caf6eed4a7452ef28a66dfaf6ebd
dhanasekaranp@lntecc.com <dhanasekaranp@lntecc.com>
2019-03-12 10:05:32 +0530

    word corrections done

--- code/data_conversion.py
+++ code/data_conversion.py
@@ -1,10 +1,37 @@
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
+import re
 
-wo_xls = pd.read_excel('../data/Sample_output_WO.xlsx', usecols = [0])
+wo_xls = pd.read_excel('./data/ltc_work_order_samples.xlsx', usecols = [1])
 wo_xls.head()
 
 # text cleaning
+# Spell-Correct words 
 
+words_to_replace = {'ani' :'any', 
+                    'concret' : 'concrete', 
+                    'charg' : 'charge', 
+                    'inclus' :'includes',
+                    'materi' : 'material',
+                    'specifi': 'specified',
+                    'centr' : 'centre',
+                    'aggreg' : 'aggregate',
+                    'posit' :'position',
+                    'provid' : 'providing',
+                    'lay' : 'laying',
+                    'exclud' : 'excluding',
+                    'center' : 'centering',
+                    'shutter' : 'shuttering',
+                    'nomin' :'nominal'}
 
+def words_correction(document):
+    for doc in document.strip().split():
+        if doc in words_to_replace:
+            document = re.sub(re.compile(r'\b%s\b' % doc, re.I), words_to_replace.get(doc), document)
+        else:
+            document
+    return document
+            
+            
+        
